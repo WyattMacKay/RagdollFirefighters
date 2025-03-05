8bea352a687f256f1436333d71599ae29648db73
WyattMacKay <wyattmackaydawson@outlook.com>
2025-03-05 13:01:08 -0400

    modify scripts to be more independent.

--- scenes/item/item.gd
+++ scenes/item/item.gd
@@ -2,12 +2,22 @@ extends Node
 
 class_name Item
 
+enum HOLD_TYPE {
+	LEFT,
+	RIGHT,
+	CENTER,
+}
+
 @export var model := "res://placeholder-models/ball.tscn"
 
 
+var node: Node
+
 func _ready() -> void:
 	var model_scene := load(model)
-	self.add_child(model_scene.instantiate())
+	if model_scene:
+		node = model_scene.instantiate()
+		self.add_child(node)
 
 func use() -> void:
 	push_error("Child classes must overwrite the use() function.")
--- scenes/player/hand.gd
+++ scenes/player/hand.gd
@@ -1,13 +1,10 @@
 extends Node3D
 
-@export var item_scene := "res://scenes/item/item.tscn"
+var item: Item
 
-var scene := load(item_scene)
-var item: Node = scene.instantiate()
-
-func _ready() -> void:
-	self.add_child(item)		#Need to make item 90 deg. and at same position as parent.
-	
+func pick_up(new_item: Item):
+	item = new_item
+	item.reparent(self)
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("attack"):
